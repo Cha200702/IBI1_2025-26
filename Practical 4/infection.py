5944e1e4a3b2efd97037ebf38d722c9db77ccf84
徐睿杰 <ruijie.25@intl.zju.edu.cn>
2026-05-01 10:52:23 +0100

    Final check on infection.py to ensure everything meets marking criteria

--- Practical 4/infection.py
+++ Practical 4/infection.py
@@ -9,13 +9,15 @@ day = 1
 
 # Simulate the infection spread until all students are infected
 
-import math # "math" is imported to use the "ceil" function for rounding up the number of infected students
+import math 
+  # "math" is imported to use the "ceil" function for rounding up the number of infected students
 
 # Use a while loop to continue until all students are infected
 while initial_infected < total_students:
     print(f'Day {day}, there are {initial_infected} infected students in total.')
     initial_infected += growth_rate * initial_infected
-    initial_infected = math.ceil(initial_infected)  # "math.ceil" is used to round up to the nearest whole number of infected students
+    initial_infected = math.ceil(initial_infected)  
+      # "math.ceil" is used to round up to the nearest whole number of infected students
     day += 1
 
 # Once the loop ends, it means all students are infected
