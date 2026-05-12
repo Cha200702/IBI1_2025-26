527931898c6c631edb6397932c8d568598f4d74e
徐睿杰 <ruijie.25@intl.zju.edu.cn>
2026-05-12 03:53:41 +0100

    Final check on the protein_mass_predictor.py to ensure everything meets criteria

--- Practical 8/protein_mass_predictor.py
+++ Practical 8/protein_mass_predictor.py
@@ -15,30 +15,38 @@ amino_acid_symbol = {
     'Arginine': 'R', 'Tyrosine': 'Y', 'Tryptophan': 'W'
 }
 
+
 # Define a function that searches for the corresponding mass of desired AA.
-def get_mass(AA): # Need a parameter here
+def get_mass(AA):
     # Turn the input into the format of strings
-    input = AA.strip()
-    if input in sequence_mass:
-        return sequence_mass[input]
-    elif input in amino_acid_symbol:
-        symbol = amino_acid_symbol[input]
+    input_aa = AA.strip()
+    if input_aa in sequence_mass:
+        return sequence_mass[input_aa]
+    elif input_aa in amino_acid_symbol:
+        symbol = amino_acid_symbol[input_aa]
         return sequence_mass[symbol]
     else:
-        print(f"Unknown amino acid: '{AA}. Error!'")
+        print(f"Unknown amino acid: '{AA}'. Error!")
+        return None
+
 
 # Define another function that calculates the overall mass of protein sequence
 def calculate_mass(sequence):
-    # Give a new variable for recording
     total_mass = 0.0
     # .upper() is used to convert all lowercase letters in a string to uppercase letters
     normalized_sequence = sequence.upper()
-    for AA in normalized_sequence:
-        total_mass += get_mass(AA)
+    for aa in normalized_sequence:
+        mass = get_mass(aa)
+        if mass is None:
+            print(f"Calculation aborted due to invalid amino acid '{aa}'.")
+            return None
+        total_mass += mass
     return total_mass
+
 
 if __name__ == "__main__":
     print("-- Example for protein mass calculation --")
     test_sequence = "GA"
     mass = calculate_mass(test_sequence)
-    print(f"The protein sequence '{test_sequence}' total mass is {mass} amu.")
+    if mass is not None:
+        print(f"The protein sequence '{test_sequence}' total mass is {mass:.2f} amu.")
